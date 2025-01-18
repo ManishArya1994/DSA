83e88d14f66dc737d0afcc4ee4843d0db91b750e
Manish Arya <manisharya.abhi@gmail.com>
2025-01-18 17:25:35 +0530

    initial commit

---
+++ LocalDateTime.java
@@ -0,0 +1,8 @@
+package JavaCode;
+
+
+public class LocalDateTime {
+    public static void main(String[] args) {
+        System.out.println("Date And Time:   "+ java.time.LocalDateTime.now());
+    }
+}
